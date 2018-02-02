0c2068ba0ac8ba39cc0134eead52ecf6d2784826
near2006 <zhangminforwork@126.com>
2018-02-02 08:19:34 -0800

    1.新增一个简单的表单上传的服务器设置。 2.针对post 表格设置，可以通过curl来模拟浏览器上传文件，主要通过CURL  -F -X 等参数

--- server/defalut_app.py
+++ server/defalut_app.py
@@ -1,10 +1,31 @@
-from bottle import Bottle, run
-
+from bottle import Bottle, run, route, request
+import os.path
 app = Bottle()
 
-@app.route('/testphoto')
-@app.route('/updatephoto')
-def update_photo():
-	return "i will updata photos."
+#@app.route('/testphoto')
+@app.route('/updatephoto/<photoname>')
+def update_photo(photoname):
+	return "i will updata %s photos." % photoname
+	
+@app.post('/update/<name>')
+def update(name):
+	return static_file(name, "update/")
+
+@app.route('/test1')
+def test1():
+	return 'test.'
+	
+#一个简单页面，可以用浏览器提交文件
+#@app.route('/upload')
+#def upload():
+#	return '"<html><head></head><body><form action "/upload" method="post" enctype="multipart/form-data"><input type="file" name="photofile" /><input type="submit" value="Upload" /></form></body></html>"'
+			
+@app.route('/upload', method = 'POST')
+def do_upload():
+	upload = request.files.get('photofile')
+	name,ext = os.path.splitext(upload.filename) #split name and extern type
+	new_name = 'new'+upload.filename
+	upload.save(new_name, overwrite=True)
+	return 'OK'
 
 run(app, host='localhost', port=8080, debug=True)
--- server/readme.txt
+++ server/readme.txt
@@ -1,6 +1,10 @@
 just for testing python,
 the server is storage photo .
 
-run:
+run server:
 1. set python env: export PYTHONPATH="../platform/bottle"
 2. run #python hello.py
+
+run client:
+1. curl http://localhost:8080/upload -F "photofile=@1.txt" 
+   提交本地1.txt文件到远端，其中photofile是与里面有关联的
